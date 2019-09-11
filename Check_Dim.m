255047e8adcb760a0abb8eefaa781d18abdd58b9
Prestamo Estudiantes <prestamour@urosario.edu.co>
2019-09-11 13:02:03 -0500

    Hola

---
+++ Check_Dim.m
@@ -0,0 +1,9 @@
+function [] = Check_Dim(c,A,b,m,n)
+    %Checks if there is the same number of costs as variables, if not, throws error
+    if (size(c) ~= n)
+        error('The length of c and A are not the same!');
+    %Check if there is the same number of restrictions as elements in b, if not, throws error
+    elseif (m ~= size(b))
+        error('The length of b and A are not the same!');
+    end
+end
